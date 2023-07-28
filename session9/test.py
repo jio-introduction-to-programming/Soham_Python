2847a0f0a3c3b60c149752a88d9e0178b14a9589
Soham Sanyal <soham.s24PGAI@jioinstitute.edu.in>
2023-07-28 20:36:18 +0530

    assignment done

---
+++ session9/test.py
@@ -0,0 +1,9 @@
+from math_operation import add, sub, div, mul
+
+a = int(input("Enter number one\n"))
+b = int(input("Enter number two\n"))
+
+print(f'Addtion Result:- {add(a,b)}')
+print(f'Multiplication Result:- {mul(a,b)}')
+print(f'Subtraction Result:- {sub(a,b)}')
+print(f'Division Result:- {div(a,b)}')
